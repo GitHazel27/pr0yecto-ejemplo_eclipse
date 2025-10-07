c014c8e4f66cac0f52ac940ae2beca440f66be15
fca <fca@FCA-CC-B-07.inet.uady.mx>
2025-10-07 10:54:29 -0600

    Boton con rama desde eclipse

--- src/ejemplo/GUI_git.java
+++ src/ejemplo/GUI_git.java
@@ -36,7 +36,7 @@ public class GUI_git extends JFrame {
 	 */
 	public GUI_git() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 485, 402);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -61,25 +61,43 @@ public class GUI_git extends JFrame {
 		btnNewButton_1.setBounds(137, 81, 142, 23);
 		contentPane.add(btnNewButton_1);
 		
-<<<<<<< HEAD
+		
 		JButton btnNewButton_2 = new JButton("Hazel");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(null, "Hola desde la rama de Haz");
 			}
 		});
-		btnNewButton_2.setBounds(51, 193, 85, 21);
+		btnNewButton_2.setBounds(137, 197, 85, 21);
 		contentPane.add(btnNewButton_2);
-=======
-		JButton btnNewButton_2 = new JButton("Rama Jimena");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		JButton btnNewButton_3 = new JButton("Rama Jimena");
+		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(null, "Hola desde mi rama");
 			}
 		});
-		btnNewButton_2.setBounds(137, 158, 142, 23);
-		contentPane.add(btnNewButton_2);
+		btnNewButton_3.setBounds(137, 158, 142, 23);
+		contentPane.add(btnNewButton_3);
 		
->>>>>>> refs/heads/RamaJimena
+		JButton btnNewButton_5 = new JButton("Jimena boton 5 ");
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Hola de mi rama, desde eclipse");
+			}
+			
+		});
+		btnNewButton_5.setBounds(137, 229, 160, 23);
+		contentPane.add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("boton jimena 2");
+		btnNewButton_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Hola boton 2 jimena, desde eclipse");
+			}
+		});
+		btnNewButton_6.setBounds(147, 273, 160, 23);
+		contentPane.add(btnNewButton_6);
+		
+
 	}
 }
